ff267739d9f785ee4bda4cbd7c28341b4561ee70
Franklin Strube <thedoc8786@gmail.com>
2012-10-22 01:30:06 -0400

    fixing environment detection for sinatra/reloader

--- flippbook.rb
+++ flippbook.rb
@@ -1,5 +1,5 @@
 require "sinatra"
-require "sinatra/reloader" if development?
+require "sinatra/reloader" if settings.environment == :development
 require "aws/s3"
 
 # Setup Amazon S3 using Heroku config
